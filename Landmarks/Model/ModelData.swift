d874da5cfa5eadb3eb5d6f9da0e5f003c3538f6d
sgs <samuel_smith@outlook.com>
2021-07-16 17:30:40 +0100

    Composing complex interfaces

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -11,7 +11,20 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json") //never modify hike data after initially loading it, don’t need to mark it with the @Published attribute.
+    
+    var features: [Landmark] {
+           landmarks.filter { $0.isFeatured }
+       }
+    
+    var categories: [String: [Landmark]] {
+          Dictionary(
+              grouping: landmarks,
+              by: { $0.category.rawValue }
+          )
+      }
 }
+
+
 
 
 
