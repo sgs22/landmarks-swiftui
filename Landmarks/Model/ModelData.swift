e2c6aff33e2a31fe97860ff7c37138887822d02e
sgs <samuel_smith@outlook.com>
2021-07-16 15:52:28 +0100

    animating views and transitions

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json") //never modify hike data after initially loading it, don’t need to mark it with the @Published attribute.
 }
 
 
