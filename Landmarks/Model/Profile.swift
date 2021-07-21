a390d7a52dd7c2364af89678d50394574880f10f
sgs <samuel_smith@outlook.com>
2021-07-21 14:10:01 +0100

    Add Profile view - Working with UI controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Samuel Smith on 19/07/2021.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+            
+    }
+}
